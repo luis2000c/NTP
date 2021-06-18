833184c839e9725105da6f0f4e7d13f662cda72b
luis2000c <59964726+luis2000c@users.noreply.github.com>
2021-06-18 12:31:50 -0500

    Consulta las funciones de console.*

--- Modulo_1/Basic/HolaMundo.js
+++ Modulo_1/Basic/HolaMundo.js
@@ -1,9 +1,20 @@
 console.log("Hola Mundo");
 //Consultar las funciones console.*
 console.error("Este es un error");
-
+//console._exception()//Es un alias de error
 
 console.info("Muestra mensajes de información")
 console.warn("Muestra información de advertencia")
 
 //console.clear()	//Limpia la concola
+
+console.assert() //registra un mensaje y envia una traza de error a la consola sie el promer argumerto es false
+console.count() // registra el número de veces que esta linea ha sido llanada con etiqueta dada
+console.debug() (en-US) //Registra un mensaje con el nivel de "debug"
+console.dir() //Muestra un listado interactivo de las propiedades de un objeto JS Específico
+console.dirxml()//Muestra una representación en form de arból de un elemento XML/HTML si es posible o la ista del objeto
+console.group() (en-US)//crea un nuevo grupo, indentando los mesajes subsecuentes en uun nuevo nivel
+console.groupEnd() (en-US)//Finaliza el grupo actual
+console.table()//Muestra datos tabulares en forma de una tabla
+console.time()//Inicia un temporizador especificado y registra el tiempo transcurrido desde su inicio
+console.trace()//Muestra una traza de error

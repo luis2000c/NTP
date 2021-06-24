713d4e7f897956a40010f63429797121efbfed45
luis2000c <59964726+luis2000c@users.noreply.github.com>
2021-06-24 12:08:21 -0500

    Servicios web(query, params)

--- Modulo_1/Programming/01_Projects/index.js
+++ Modulo_1/Programming/01_Projects/index.js
@@ -12,10 +12,38 @@ const express = require('express');
 //Genero una aplicación express
 const app = express()
 //EndPoint
+//Nodemon va a mantener vivo el programa y mantener los cambios visibles
+//npm i -D -E nodemon
 app.get('/',(req,res) => {
-    res.send("Hola Mundo con ExpressJS")
-}).listen(3000,() => {
+    res.send("Hola Mundo")
+})
+app.get("/saludo",(req,res)=>{
+    //req .query -accedo a todo lo que venga en el query
+    console.log(req.query);
+    const {query:{nombre, apellido}} = req
+    //const nombre = req.query.nombre
+    //const nombre = req.query.apellido
+    res.json({
+        "Saludo":`Hola soy ${nombre}  ${apellido}`,
+    });
+});
+//Parámetro
+app.get("/saludo/:nombre",(req,res)=>{
+    console.log(req);
+    console.log(req.params);
+    const {params:{nombre}} = req
+    //const nombre = req.params.nombre
+    res.json({
+        nombre:nombre,
+    })
+})
+
+app.listen(3000,() => {
     console.log("Servidor escuchando en http://localhost:3000");
 })
+
+//Datos para enviar al servidor
+//query-
+//params
 
 
---
+++ Modulo_1/Programming/01_Projects/routes/index.js
@@ -0,0 +1,14 @@
+//creacion del router
+///Traemos el paquete
+const { Router } = require('express');
+///Iniciamos Router
+const router = Router()
+
+
+
+//implementacion
+
+
+///Exportamos los archivos de rutas
+module .exports.RouterIndex = router
+//midelwere con express

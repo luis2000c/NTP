81bd92a94c95df9ea0b4271df648173da99d6d18
luis2000c <59964726+luis2000c@users.noreply.github.com>
2021-06-25 12:07:22 -0500

    Manejo de rutas

--- Modulo_1/Programming/01_Projects/index.js
+++ Modulo_1/Programming/01_Projects/index.js
@@ -14,30 +14,40 @@ const app = express()
 //EndPoint
 //Nodemon va a mantener vivo el programa y mantener los cambios visibles
 //npm i -D -E nodemon
-app.get('/',(req,res) => {
-    res.send("Hola Mundo")
-})
-app.get("/saludo",(req,res)=>{
-    //req .query -accedo a todo lo que venga en el query
-    console.log(req.query);
-    const {query:{nombre, apellido}} = req
-    //const nombre = req.query.nombre
-    //const nombre = req.query.apellido
-    res.json({
-        "Saludo":`Hola soy ${nombre}  ${apellido}`,
-    });
-});
-//Parámetro
-app.get("/saludo/:nombre",(req,res)=>{
-    console.log(req);
-    console.log(req.params);
-    const {params:{nombre}} = req
-    //const nombre = req.params.nombre
-    res.json({
-        nombre:nombre,
-    })
-})
+//EL INDEX ANTES
+// app.get('/',(req,res) => {
+//     res.send("Hola Mundo")
+// })
+// app.get("/saludo",(req,res)=>{
+//     //req .query -accedo a todo lo que venga en el query
+//     console.log(req.query);
+//     const {query:{nombre, apellido}} = req
+//     //const nombre = req.query.nombre
+//     //const nombre = req.query.apellido
+//     res.json({
+//         "Saludo":`Hola soy ${nombre}  ${apellido}`,
+//     });
+// });
+// //Parámetro
+// app.get("/saludo/:nombre",(req,res)=>{
+//     console.log(req);
+//     console.log(req.params);
+//     const {params:{nombre}} = req
+//     //const nombre = req.params.nombre
+//     res.json({
+//         nombre:nombre,
+//     })
+// })
 
+const { RouterIndex } = require('./routes/index')
+
+app.use("/", RouterIndex)
+// app.use("/user",RouterIndex)
+// app.use("/user/data",RouterIndex)
+/**
+ "/" + "/" = "//" => "/"
+ "/" + "/saludo" => "//saludo" => "/saludo"
+ */
 app.listen(3000,() => {
     console.log("Servidor escuchando en http://localhost:3000");
 })
--- Modulo_1/Programming/01_Projects/routes/index.js
+++ Modulo_1/Programming/01_Projects/routes/index.js
@@ -2,13 +2,47 @@
 ///Traemos el paquete
 const { Router } = require('express');
 ///Iniciamos Router
+const data = require("./data.json")
 const router = Router()
 
+//Creacion de endpoint
+router
+    .get("/",(req,res) => {    
+        res.send("Hola Mundo")
+    })
+    .get("/saludo",(req,res)=>{
+        //req .query -accedo a todo lo que venga en el query
+        console.log(req.query);
+        const {query:{nombre, apellido}} = req
+        //const nombre = req.query.nombre
+        //const nombre = req.query.apellido
+        res.json({
+            "Saludo":`Hola soy ${nombre}  ${apellido}`,
+        });
+    })
+    .get("/saludo/:nombre",(req,res)=>{
+        //console.log(req);
+        console.log(req.params);
+        const {params:{nombre}} = req
+        //const nombre = req.params.nombre
+        res.json({
+            nombre:nombre,
+        })
+    })
+    .get("/users",(req,res)=>{
+        res.json{
+           msg : "Lista de usuarios ",
+        
+
+        }
+        
+
+    })
 
 
 //implementacion
 
 
 ///Exportamos los archivos de rutas
-module .exports.RouterIndex = router
-//midelwere con express
+module.exports.RouterIndex = router
+//midelwere con express(REVISAR)
